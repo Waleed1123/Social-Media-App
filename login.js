d7ae9d9d3ad8393e5dbeb1134667f149222f685c
Waleed1123 <waleed007896@gmail.com>
2023-07-03 02:22:45 +0500

    JavaScript Problem But Half Done

---
+++ login.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function () {
+  console.log(localStorage.getItem("loginUser"))
+  var userLogin = localStorage.getItem("loginUser")
+  if (userLogin) {
+      window.location.replace("./dashboard.html")
+  }
+
+})
+
+
+function login() {
+  var email = document.getElementById("email").value
+  var password = document.getElementById("password").value
+
+  var getUser = JSON.parse(localStorage.getItem("users"))
+
+  var user = getUser.find(function (value) {
+      if (value.email === email && value.password === password) return true
+
+  })
+
+  if (user !== -1) {
+      console.log("successfully login")
+      alert("successfully login")
+      localStorage.setItem("loginUser", JSON.stringify(user))
+      window.location.replace("./dashboard.html")
+  } else {
+      console.log("email OR password does not match")
+      alert("email OR password does not match")
+  }
+
+
+}
